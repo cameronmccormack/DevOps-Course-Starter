bba090fd2075cb1bb8b72178370925b81a286dbc
Cameron McCormack <cameron.mccormack@softwire.com>
2021-10-22 17:05:25 +0100

    Ex1: implement changes for base exercise

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask import render_template, redirect
+from flask.globals import request
 
+from todo_app.data import session_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,4 +11,11 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session_items.get_items()
+    return render_template('index.html', items=items)
+
+@app.route('/addItem', methods=['POST'])
+def add_item():
+    item = request.form.get('add-item')
+    session_items.add_item(item)
+    return redirect('/')
