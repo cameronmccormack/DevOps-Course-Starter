d0ac2760b8dc754b7f2a60cada766e87ef981f35
Cameron McCormack <cameron.mccormack@softwire.com>
2021-10-26 10:11:24 +0100

    Ex1: extension work - restyle and add functionality

--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,4 +1,6 @@
 from flask import session
+from todo_app.models.status import Status
+
 
 def get_items():
     """
@@ -8,6 +10,27 @@ def get_items():
         list: The list of saved items.
     """
     return session.get('items', [])
+
+
+def get_items_by_status():
+    """
+    Fetches all saved items from the session, partitioned by status.
+
+    Returns:
+        not_started: The list of items with Not Started status
+        in_progress: The list of items with In Progress status
+        done: The list of items with Done status
+    """
+    items = get_items()
+    not_started, in_progress, done = [], [], []
+    for item in items:
+        if (item['status'] == Status.DONE):
+            done.append(item)
+        elif (item['status'] == Status.IN_PROGRESS):
+            in_progress.append(item)
+        else:
+            not_started.append(item)
+    return not_started, in_progress, done
 
 
 def get_item(id):
@@ -39,7 +62,7 @@ def add_item(title):
     # Determine the ID for the item based on that of the previously added item
     id = items[-1]['id'] + 1 if items else 0
 
-    item = { 'id': id, 'title': title, 'status': 'Not Started' }
+    item = {'id': id, 'title': title, 'status': Status.NOT_STARTED}
 
     # Add the item to the list
     items.append(item)
@@ -50,14 +73,31 @@ def add_item(title):
 
 def save_item(item):
     """
-    Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
+    Updates an existing item in the session. If no existing item matches the
+    ID of the specified item, nothing is saved.
 
     Args:
         item: The item to save.
     """
     existing_items = get_items()
-    updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
+    updated_items = [
+        item if item['id'] == existing_item['id']
+        else existing_item for existing_item in existing_items
+    ]
 
     session['items'] = updated_items
 
     return item
+
+
+def delete_item(id):
+    """
+    Deletes the item with the specified ID from the session.
+
+    Args:
+        id: The ID of the item.
+    """
+    existing_items = get_items()
+    updated_items = [item for item in existing_items if item['id'] != int(id)]
+
+    session['items'] = updated_items
---
+++ todo_app/models/status.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+
+class Status(IntEnum):
+    NOT_STARTED = 1
+    IN_PROGRESS = 2
+    DONE = 3
