a06850a969a1f85b8b67f989c4f295ffa0abf822
cameronmccormack <39823505+cameronmccormack@users.noreply.github.com>
2021-11-03 08:48:11 +0000

    Exercise 1 (#1)
    * Ex1: implement changes for base exercise
    
    * Ex1: remove default items and add message for when list is empty
    
    * Ex1: extension work - restyle and add functionality
    
    * Ex1: use list comprehensions for partitioning by status
    
    Co-authored-by: JackMead <Jack.Mead@softwire.com>
    
    Co-authored-by: JackMead <Jack.Mead@softwire.com>

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import render_template, redirect
+from flask.globals import request
 
+from todo_app.data import session_items
 from todo_app.flask_config import Config
+from todo_app.models.status import Status
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +12,47 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    not_started, in_progress, done = session_items.get_items_by_status()
+    return render_template(
+        'index.html',
+        not_started=not_started,
+        in_progress=in_progress,
+        done=done
+    )
+
+
+@app.route('/addItem', methods=['POST'])
+def add_item():
+    item = request.form.get('add-item')
+    session_items.add_item(item)
+    return redirect('/')
+
+
+@app.route('/markNotStarted/<id>', methods=['POST'])
+def mark_not_started(id):
+    item = session_items.get_item(id)
+    item['status'] = Status.NOT_STARTED
+    session_items.save_item(item)
+    return redirect('/')
+
+
+@app.route('/markInProgress/<id>', methods=['POST'])
+def mark_in_progress(id):
+    item = session_items.get_item(id)
+    item['status'] = Status.IN_PROGRESS
+    session_items.save_item(item)
+    return redirect('/')
+
+
+@app.route('/markDone/<id>', methods=['POST'])
+def mark_done(id):
+    item = session_items.get_item(id)
+    item['status'] = Status.DONE
+    session_items.save_item(item)
+    return redirect('/')
+
+
+@app.route('/delete/<id>', methods=['POST'])
+def delete_item(id):
+    session_items.delete_item(id)
+    return redirect('/')
--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,9 +1,5 @@
 from flask import session
-
-_DEFAULT_ITEMS = [
-    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
-]
+from todo_app.models.status import Status
 
 
 def get_items():
@@ -13,7 +9,23 @@ def get_items():
     Returns:
         list: The list of saved items.
     """
-    return session.get('items', _DEFAULT_ITEMS.copy())
+    return session.get('items', [])
+
+
+def get_items_by_status():
+    """
+    Fetches all saved items from the session, partitioned by status.
+
+    Returns:
+        not_started: The list of items with Not Started status
+        in_progress: The list of items with In Progress status
+        done: The list of items with Done status
+    """
+    items = get_items()
+    not_started = [item for item in items if item['status'] == Status.NOT_STARTED]
+    in_progress = [item for item in items if item['status'] == Status.IN_PROGRESS]
+    done = [item for item in items if item['status'] == Status.DONE]
+    return not_started, in_progress, done
 
 
 def get_item(id):
@@ -45,7 +57,7 @@ def add_item(title):
     # Determine the ID for the item based on that of the previously added item
     id = items[-1]['id'] + 1 if items else 0
 
-    item = { 'id': id, 'title': title, 'status': 'Not Started' }
+    item = {'id': id, 'title': title, 'status': Status.NOT_STARTED}
 
     # Add the item to the list
     items.append(item)
@@ -56,14 +68,31 @@ def add_item(title):
 
 def save_item(item):
     """
-    Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
+    Updates an existing item in the session. If no existing item matches the
+    ID of the specified item, nothing is saved.
 
     Args:
         item: The item to save.
     """
     existing_items = get_items()
-    updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
+    updated_items = [
+        item if item['id'] == existing_item['id']
+        else existing_item for existing_item in existing_items
+    ]
 
     session['items'] = updated_items
 
     return item
+
+
+def delete_item(id):
+    """
+    Deletes the item with the specified ID from the session.
+
+    Args:
+        id: The ID of the item.
+    """
+    existing_items = get_items()
+    updated_items = [item for item in existing_items if item['id'] != int(id)]
+
+    session['items'] = updated_items
