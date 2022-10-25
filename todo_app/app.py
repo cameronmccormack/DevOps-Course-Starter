f3484717bddd257777e08746c02e3e2c02c25811
Cameron McCormack <cameron.mccormack@softwire.com>
2022-10-25 17:50:21 +0100

    ex-11: add authentication

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,19 +1,33 @@
 from flask import Flask
 from flask import render_template, redirect
 from flask.globals import request
+from flask_login import LoginManager, login_required, login_user
+import requests
 
 from todo_app.data.mongo_db_items import MongoDbItemProvider as ItemProvider
 from todo_app.flask_config import Config
 from todo_app.models.status import Status
 from todo_app.models.view_model import ViewModel
+from todo_app.auth.user import User
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config())
+    config = Config()
+    app.config.from_object(config)
     item_provider = ItemProvider()
+    login_manager = LoginManager()
+
+    @login_manager.unauthorized_handler
+    def unauthenticated():
+        return redirect(config.GITHUB_LOGIN_URL)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User(user_id)
 
     @app.route('/')
+    @login_required
     def index():
         not_started, in_progress, done = item_provider.get_items_by_status()
         show_all_items = (
@@ -28,37 +42,72 @@ def create_app():
         return render_template('index.html', view_model=item_view_model)
 
     @app.route('/showHiddenItems/')
+    @login_required
     def show_hidden_items():
         return redirect('/?show_all_items=true')
 
     @app.route('/hideExpandedItems/')
+    @login_required
     def hide_exapanded_items():
         return redirect('/?show_all_items=false')
 
     @app.route('/addItem', methods=['POST'])
+    @login_required
     def add_item():
         item = request.form.get('add-item')
         item_provider.add_item(item)
         return redirect('/')
 
     @app.route('/markNotStarted/<id>', methods=['POST'])
+    @login_required
     def mark_not_started(id):
         item_provider.update_status(id, Status.NOT_STARTED)
         return redirect('/')
 
     @app.route('/markInProgress/<id>', methods=['POST'])
+    @login_required
     def mark_in_progress(id):
         item_provider.update_status(id, Status.IN_PROGRESS)
         return redirect('/')
 
     @app.route('/markDone/<id>', methods=['POST'])
+    @login_required
     def mark_done(id):
         item_provider.update_status(id, Status.DONE)
         return redirect('/')
 
     @app.route('/delete/<id>', methods=['POST'])
+    @login_required
     def delete_item(id):
         item_provider.delete_item(id)
         return redirect('/')
 
+    @app.route('/login/callback')
+    def user_login():
+        auth_params = {
+            "client_id": config.GITHUB_CLIENT_ID,
+            "client_secret": config.GITHUB_CLIENT_SECRET,
+            "code": request.args.get("code")
+        }
+
+        access_token = requests.post(
+            "https://github.com/login/oauth/access_token",
+            params=auth_params,
+            headers={"Accept": "application/json"}
+        ).json()["access_token"]
+
+        user_id = requests.get(
+            "https://api.github.com/user",
+            headers={
+                "Accept": "application/json",
+                "Authorization": f"Bearer {access_token}"
+            }
+        ).json()["id"]
+
+        user = User(user_id)
+        login_user(user)
+
+        return redirect('/')
+
+    login_manager.init_app(app)
     return app
--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -6,7 +6,25 @@ class Config:
         """Base configuration variables."""
         self.SECRET_KEY = os.environ.get('SECRET_KEY')
         if not self.SECRET_KEY:
-            raise ValueError(
-                "No SECRET_KEY set for Flask application. " +
+            self.throw_missing_env_error('SECRET_KEY')
+
+        self.GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
+        if not self.GITHUB_CLIENT_ID:
+            self.throw_missing_env_error('GITHUB_CLIENT_ID')
+
+        self.GITHUB_LOGIN_URL = (
+            "https://github.com/login/oauth/authorize" +
+            f"?client_id={self.GITHUB_CLIENT_ID}"
+        )
+
+        self.GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
+        if not self.GITHUB_CLIENT_SECRET:
+            self.throw_missing_env_error('GITHUB_CLIENT_SECRET')
+
+        self.LOGIN_DISABLED = os.environ.get('LOGIN_DISABLED') == 'True'
+
+    def throw_missing_env_error(key):
+        raise ValueError(
+                f"No {key} set for Flask application. " +
                 "Did you follow the setup instructions?"
             )
